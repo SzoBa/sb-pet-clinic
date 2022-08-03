b64032bd4750b5364b13148b89ee9491188247ed
Balázs Szolcsánszki <bszolcsan@gmail.com>
2022-08-03 10:37:23 +0200

    Refactor services

--- pet-clinic-data/src/main/java/com/springpractice/sbpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/springpractice/sbpetclinic/services/OwnerService.java
@@ -2,13 +2,9 @@ package com.springpractice.sbpetclinic.services;
 
 import com.springpractice.sbpetclinic.model.Owner;
 
-import java.util.Set;
 
-public interface OwnerService {
+public interface OwnerService extends CrudService<Owner, Long>{
 
     Owner findByLastName(String lastName);
-    Owner findById(Long id);
-    Owner save(Owner owner);
-    Set<Owner> findAll();
 
 }
--- pet-clinic-data/src/main/java/com/springpractice/sbpetclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/com/springpractice/sbpetclinic/services/VetService.java
@@ -2,12 +2,7 @@ package com.springpractice.sbpetclinic.services;
 
 import com.springpractice.sbpetclinic.model.Vet;
 
-import java.util.Set;
 
-public interface VetService {
-
-    Vet findById(Long id);
-    Vet save(Vet pet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long> {
 
 }
