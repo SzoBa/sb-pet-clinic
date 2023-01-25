ad8f4c50f2d49ad941ff8fcc518b236be9420209
Balázs Szolcsánszki <bszolcsan@gmail.com>
2023-01-25 20:17:39 +0100

    Add vets list Closes #14

--- pet-clinic-web/src/main/java/com/springpractice/sbpetclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/springpractice/sbpetclinic/controllers/VetController.java
@@ -1,13 +1,27 @@
 package com.springpractice.sbpetclinic.controllers;
 
+import com.springpractice.sbpetclinic.model.BaseEntity;
+import com.springpractice.sbpetclinic.services.VetService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 @Controller
 public class VetController {
 
+    @Autowired
+    private VetService vetService;
+
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(Model model) {
+
+        model.addAttribute("vets", vetService.findAll().stream().sorted(Comparator.comparing(BaseEntity::getId)).collect(Collectors.toList()));
+
         return "vets/index";
     }
 
