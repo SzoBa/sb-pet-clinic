51d9bea352b8da0d2ed959df306f4850e6253d5e
Balázs Szolcsánszki <bszolcsan@gmail.com>
2023-02-12 00:42:59 +0100

    Add Speciality service

---
+++ pet-clinic-data/src/main/java/com/springpractice/sbpetclinic/services/SpecialityService.java
@@ -0,0 +1,6 @@
+package com.springpractice.sbpetclinic.services;
+
+import com.springpractice.sbpetclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Long> {
+}
